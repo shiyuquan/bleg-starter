42a6965d3ae3dafec9611114067fe0ee1587bf2b
shiyuquan <550765252@qq.com>
2020-07-29 18:20:06 +0800

    Initial commit

---
+++ src/main/java/org/bleg/blegstarter/BlegStarterApplication.java
@@ -0,0 +1,13 @@
+package org.bleg.blegstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlegStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlegStarterApplication.class, args);
+    }
+
+}
